6d6b17d73fa4ec85e4a9e95e92602fa21a847609
Jerry <az.dev@88.com>
2024-11-10 18:19:15 +0800

    Scanning device

---
+++ lib_common/src/main/ets/constant/SizeConstant.ts
@@ -0,0 +1,56 @@
+export class SizeConstant {
+  // 标题栏高度
+  static readonly APP_BAR_HEIGHT = 38
+
+  static readonly DRIVER = '1px'
+
+
+  // 文本大小相关
+  // 小号文本
+  static readonly TEXT_S = 10
+  // 中号
+  static readonly TEXT_M = 12
+  // 大号文本
+  static readonly TEXT_L = 14
+  // X大号文本
+  static readonly TEXT_XL = 16
+  // XX大号文本
+  static readonly TEXT_XXL = 18
+
+
+  // 图标大小相关
+  // 小号图标
+  static readonly ICON_S = 13
+  // 中号图标
+  static readonly ICON_M = 16
+  // 大号图标
+  static readonly ICON_L = 19
+  // X大号图标
+  static readonly ICON_XL = 24
+  // XX大号图标
+  static readonly ICON_XXL = 32
+
+  // 圆角大小相关
+  // 小号圆角
+  static readonly RADIUS_S = 3
+  // 中号圆角
+  static readonly RADIUS_M = 5
+  // 大号圆角
+  static readonly RADIUS_L = 14
+
+  // 间距相关
+  // 小号间距
+  static readonly SPACE_EXTRA_S = 3
+  // 小号间距
+  static readonly SPACE_S = 5
+  // 中号间距
+  static readonly SPACE_M = 10
+  // 大号间距
+  static readonly SPACE_L = 14
+  // X大号间距
+  static readonly SPACE_XL = 18
+  // XX大号间距
+  static readonly SPACE_XXL = 26
+}
+
+
